f8b59e9545f6afa24f10bdfaf69b1879793f0308
velimd <duzgunce.vd@gmail.com>
2019-11-20 19:29:53 +0000

    assigning ace correct initial value

--- Blackjack/Deck.swift
+++ Blackjack/Deck.swift
@@ -11,10 +11,10 @@ import Foundation
 class Deck {
     var cards: [Card] = []
     // Veli - I still dont think we should have titles as an enum in the Card class, if we do we need to include the numbers as well imo
-    let titles = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", Card.titles.jack.rawValue, Card.titles.queen.rawValue, Card.titles.king.rawValue, Card.titles.ace.rawValue]
+    let titles = [Card.titles.ace.rawValue, "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", Card.titles.jack.rawValue, Card.titles.queen.rawValue, Card.titles.king.rawValue]
 
     init() {
-        for j in 2...14 {
+        for j in 1...13 {
           cards.append(Card(value: j, suit: Card.suit.spades.rawValue, title: String(titles[j])))
           cards.append(Card(value: j, suit: Card.suit.diamonds.rawValue, title: String(titles[j])))
           cards.append(Card(value: j, suit: Card.suit.clubs.rawValue, title: String(titles[j])))
