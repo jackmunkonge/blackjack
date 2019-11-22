9676c77ba844477a5348a61c19582d1e8cf69934
Jack Munkonge <jackmunkonge@gmail.com>
2019-11-22 23:13:07 +0000

    Finish hand model

--- Blackjack/Model/Hand.swift
+++ Blackjack/Model/Hand.swift
@@ -8,12 +8,52 @@
 
 import Foundation
 
-class Hand {
-    let cards: [Card]
+final class Hand {
+    public private(set) var cards: [Card] {
+        didSet {
+            calculateHandValues()
+        }
+    }
+    public private(set) var flopped = false
+    public private(set) var softValue = 0
+    public private(set) var hardValue = 0
     
     init() {
-        let randomCard = Card()
-        self.cards = [randomCard, randomCard]
+        self.cards = [Card(), Card()]
+    }
+    
+    init(fromDeck deck: Deck) {
+        self.cards = [deck.getNext(), deck.getNext()]
+    }
+    
+    public func flop() {
+        for i in 0..<cards.count {
+            self.cards[i].faceDown()
+        }
+    }
+    
+    public func showHand() {
+        for i in 0..<cards.count {
+            self.cards[i].faceUp()
+        }
+    }
+    
+    private func calculateHandValues() {
+        var softValue = 0
+        var hardValue = 0
+        
+        for card in cards {
+            if card.title == .ace {
+                softValue += 1
+                hardValue += 11
+            } else {
+                softValue += card.value
+                hardValue += card.value
+            }
+        }
+        
+        self.softValue = softValue
+        self.hardValue = hardValue
     }
 }
 
